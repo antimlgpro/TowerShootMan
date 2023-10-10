1fe2ced5ef362d3acf225f6e599bb3bd1b2d9da4
Ludvig <ludvigvonreis@gmail.com>
2023-10-10 14:02:56 +0200

    Added beginings of modular towers

---
+++ Assets/Scripts/Tower/TowerBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO: This will contain base behaviour for a tower. 
+// Ie. a projectile behaviour will contain logic for projectile based towers.
+
+public abstract class TowerBehaviour : ScriptableObject
+{
+	
+}
